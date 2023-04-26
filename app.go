0401e3907a3a85ad5d11a153785df74957d8e418
Felipe8617 <felipe8617@gmail.com>
2023-04-25 20:22:19 -0500

    first commit

--- app.go
+++ app.go
@@ -16,7 +16,7 @@ func Index(rw http.ResponseWriter, r *http.Request) {
 
 	template, err := template.ParseFiles("templates/index.html")
 
-	user := Users{"Felipe Carvajal", "felipe@mail.com", 123456}
+	user := Users{"Felipe Carvajal", "felipe@mail.com", 1234567}
 
 	if err != nil {
 		panic(err)
