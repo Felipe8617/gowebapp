01fd1f8f3fe25d7913a63128cd1328836579d65d
Felipe8617 <felipe8617@gmail.com>
2023-05-06 18:35:22 -0500

    changing number

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func Index(rw http.ResponseWriter, r *http.Request) {
 
 	template, err := template.ParseFiles("templates/index.html")
 
-	user := Users{"Felipe Carvajal", "felicia@mail.com", 16999}
+	user := Users{"Felipe Carvajal", "felicia@mail.com", 456787313}
 
 	if err != nil {
 		panic(err)
