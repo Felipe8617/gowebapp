42e2726b5afb0bb11480bc67f8691d3aa3355420
Felipe8617 <felipe8617@gmail.com>
2023-05-29 16:24:48 -0500

    changes

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type Users struct {
 var InfoUser = Users{
 	Name:  "Daniela",
 	Email: "juan@mail.com",
-	Phone: 888888,
+	Phone: 800088888,
 }
 
 func Index(rw http.ResponseWriter, r *http.Request) {
