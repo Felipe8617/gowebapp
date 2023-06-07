6279818129371d3cbb5a2f113d247259bfd8e83a
Felipe8617 <felipe8617@gmail.com>
2023-06-06 22:38:50 -0500

    bugs golang

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 type Users struct {
@@ -18,39 +17,48 @@ var InfoUser = Users{
 	Phone: 3669977,
 }
 
-var InfoUser = Users{
-	Name:  "Mary",
-	Email: "maary@mail.com",
-	Phone: 3669977,
-}
+// func Index(rw http.ResponseWriter, r *http.Request) {
 
-func Index(rw http.ResponseWriter, r *http.Request) {
+// 	template, err := template.ParseFiles("templates/index.html")
 
-	template, err := template.ParseFiles("templates/index.html")
+// 	user := Users{InfoUser.Name, InfoUser.Email, InfoUser.Phone}
 
-	user := Users{InfoUser.Name, InfoUser.Email, InfoUser.Phone}
+// 	if err != nil {
+// 		panic(err)
+// 	} else {
+// 		template.Execute(rw, user)
+// 	}
+// }
 
-	if err != nil {
-		panic(err)
-	} else {
-		template.Execute(rw, user)
-	}
-}
+// func main() {
 
-func main() {
+// 	http.HandleFunc("/", Index)
 
-	http.HandleFunc("/", Index)
+// 	//creating the server
+// 	fmt.Println("server is runing on port 3000")
+// 	fmt.Println("Run server: http://localhost:3000")
+// 	http.ListenAndServe(":3000", nil)
 
-	//creating the server
-	fmt.Println("server is runing on port 3000")
-	fmt.Println("Run server: http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
-
-}
+// }
 
 func bug() {
 
 	var a int = 10
 	var b int = 0
 	fmt.Println(a / b)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	// Bug: no response sent to the client
+
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", Index)
+
+	// Creación del servidor
+	fmt.Println("Server is running on port 3000")
+	fmt.Println("Run server: http://localhost:3000")
+	http.ListenAndServe(":3000", nil)
 }
